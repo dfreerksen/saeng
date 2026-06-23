4180690d9913b4557cd05e2dabd5ec5fec805549
David <dfreerksen@gmail.com>
2026-06-23 06:29:03 -0500

    Fix error on boot

--- main.js
+++ main.js
@@ -12,8 +12,6 @@ import { trustCA, untrustCA } from './src/ssl/trust.js';
 import { UpdateChecker } from './src/updateChecker.js';
 import { buildHar } from './src/proxy/har.js';
 import pkg from './package.json' with { type: 'json' };
-import bootstrapPkg from 'bootstrap/package.json' with { type: 'json' };
-import reactPkg from 'react/package.json' with { type: 'json' };
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -447,8 +445,8 @@ function setupIPC() {
     version: pkg.version,
     electron: process.versions.electron,
     node: process.versions.node,
-    bootstrap: bootstrapPkg.version,
-    react: reactPkg.version,
+    bootstrap: pkg.devDependencies.bootstrap.replace(/^[\^~]/, ''),
+    react: pkg.devDependencies.react.replace(/^[\^~]/, ''),
   }));
 
   ipcMain.handle('update:get-status', () => updateChecker.getStatus());
